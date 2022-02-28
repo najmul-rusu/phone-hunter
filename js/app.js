58467162b2f2849b33b55ae549a9aedeeb530432
najmul-rusu <nhrusu@gmail.com>
2022-02-28 23:12:30 +0600

    search aria added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('API')
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
