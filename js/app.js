82790c23e8ae4df2b5f88f398a86792c347ba66d
najmul-rusu <nhrusu@gmail.com>
2022-03-01 05:20:44 +0600

    error msg added

--- js/app.js
+++ js/app.js
@@ -2,31 +2,42 @@ const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
+    if (searchText == '') {
+        alert("Please Write Something To Display")
+    } else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchReult(data.data));
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchReult(data.data));
+    }
 }
 
 const displaySearchReult = phones => {
     const searchResult = document.getElementById('search-result');
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-100 p-3">
-            <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p>Brand : ${phone.brand}</p>
-                <button type="button" class="btn btn-info" <button type="button" class="btn btn-info" onclick="loadPhoneDetail('${phone.slug}')">Explore</button>
+    searchResult.textContent = '';
+    const errorMsg = document.getElementById('error-msg');
+    if (phones.length == 0) {
+        errorMsg.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 p-3">
+                <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p>Brand : ${phone.brand}</p>
+                    <button type="button" class="btn btn-info" <button type="button" class="btn btn-info" onclick="loadPhoneDetail('${phone.slug}')">Explore</button>
+                </div>
             </div>
-        </div>
-        `
-        searchResult.appendChild(div);
-    });
+            `
+            searchResult.appendChild(div);
+        });
+    }
+
 }
 
 const loadPhoneDetail = PhoneId => {
@@ -41,6 +52,7 @@ const loadPhoneDetail = PhoneId => {
 const displayPhoneDetail = phone => {
     console.log(phone);
     const phoneDetail = document.getElementById('phone-datil');
+    phoneDetail.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -52,19 +64,19 @@ const displayPhoneDetail = phone => {
         </div>
 
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">Main Features</li>
-            <li class="list-group-item">chipSet : ${phone.mainFeatures.chipSet}</li>
-            <li class="list-group-item">display : ${phone.mainFeatures.displaySize}</li>
+            <li class="list-group-item"><b>Main Features</b></li>
+            <li class="list-group-item">chip Set : ${phone.mainFeatures.chipSet}</li>
+            <li class="list-group-item">Display : ${phone.mainFeatures.displaySize}</li>
             <li class="list-group-item">Memory : ${phone.mainFeatures.memory}</li>
             <li class="list-group-item">Storage : ${phone.mainFeatures.storage}</li>
 
         </ul>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">Sensors</li>
+            <li class="list-group-item"><b>Sensors</b></li>
             <li class="list-group-item">${phone.mainFeatures.sensors.toString()}</li>
         </ul>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">Others</li>
+            <li class="list-group-item"><b>Others</b></li>
             <li class="list-group-item">Bluetooth : ${phone.others.Bluetooth}</li>
             <li class="list-group-item">GPS : ${phone.others.GPS}</li>
             <li class="list-group-item">NFC : ${phone.others.NFC}</li>
